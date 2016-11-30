b5b77a9c8feeada524fb47c205df6cb89114f3c2
Olivier Verdier <olivier.verdier@gmail.com>
2016-11-30 09:46:36 +0100

    more automation

--- SumInduction.v
+++ SumInduction.v
@@ -6,40 +6,45 @@ Local Open Scope R_scope.
 
 (* Solution of some standard sum induction exercises *)
 
-Ltac rec_finish IH := simpl; rewrite IH; try field; try assumption.
+Hint Resolve S_INR.
 
 Theorem sum_nat: forall n:nat, (sum (fun k => INR k) n) = INR n * (INR n-1)/2.
 Proof.
-  intros n. unfold sum.
+  intros n. 
   induction n as [| n']. 
   * simpl. field.
-  * simpl sum_from. rewrite S_INR. rec_finish IHn'.
+  * simpl sum. replace (INR (S n')) with (INR n' + 1) by intuition.  rewrite IHn'. field.
 Qed.
 
-Lemma sum_geometric: forall n x, (1 - x <> 0) -> (sum (fun k => x^k)) n = (1 - x^n)/(1-x).
+
+Lemma sum_geometric': forall m n x, (1 - x <> 0) -> sum (from (fun k => x^k) m) n = x^m * (1 - x^n)/(1-x).
 Proof.
-  intros. unfold sum.
-  induction n as [|n'].
-  * simpl. field. assumption.
-  * rec_finish IHn'.
+  intros.
+  induction n as [|n' IHn].
+  - simpl. field. trivial.
+  - simpl. rewrite IHn. unfold from. replace (x^(m+n')) with (x^m * x^n') by intuition. field; trivial. 
 Qed.
 
-Definition tmp_u (k:nat) := (2 * INR k + 1)^2.
 
-Lemma sum_odd_square: forall n, sum tmp_u n = INR n * (4*(INR n)^2 - 1)  / 3.
+Definition odd_square k := (2 * INR k + 1)^2.
+
+
+Lemma sum_odd_square: forall n, sum odd_square n = INR n * (4*(INR n)^2 - 1)  / 3.
 Proof.
-  intros. unfold sum.
+  intros.
   induction n as [|n'].
   * simpl. field.
-  * simpl sum_from. rewrite IHn'. unfold tmp_u. rewrite S_INR. field.
+  * simpl sum. rewrite IHn'. unfold odd_square. replace (INR (S n')) with (INR n' + 1) by intuition.  field.
 Qed.
 
 
 
 Theorem sum_k_Sk: forall n, sum (fun k => INR k * (INR k+1)) n = (INR n -1) * (INR n) * (INR n+1) /3.
 Proof.
-  intros. unfold sum.
+  intros.
   induction n as [|n'].
   * simpl. field.
-  * simpl sum_from. rewrite S_INR. rec_finish IHn'. 
+  * simpl sum. replace (INR (S n')) with (INR n' + 1) by intuition. rewrite IHn'. field.
 Qed.
+
+Local Close Scope R_scope.
